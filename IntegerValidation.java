5974d878aa79819f74f5a83562d699342ad97ad2
Jared Steiner (Linux Mint Acer Laptop) <jared.steiner1996@gmail.com>
2016-09-18 13:00:30 +1000

    Adding Validation to Integer inputs

---
+++ IntegerValidation.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class IntegerValidation {
+
+  // Returns boolean of whether strNum can be an Integer 
+  static boolean isInt(String strNum) throws IOException{
+    try {
+      // cast string as int
+      int intNum = Integer.parseInt(strNum);
+      // if it doesn"t throw an error, return TRUE
+      return true;
+    }
+    catch (NumberFormatException nFE) {
+      return false;
+    }
+  }
+
+  // returns String number as Int, expected isInt used to verify, no check in here
+  static int castAsInt(String strNum) {
+    return Integer.parseInt(strNum);
+  }
+
+}
